077eb202a3432ada831c32c546c3cfbef0330fd0
Elizabeth Parisi <parisielizabeth@gmail.com>
2018-12-17 04:29:56 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  array = [0, 1, 2, 3, 4, 5]
+  array = [9, , 2, 3, 4, 5]
   array.each do |item|
-  item = item + 2
-  puts "The current item + 2 is #{item}."
-end
+    item = item + 2
+    puts "The current item + 2 is #{item}."
+  end
 end
